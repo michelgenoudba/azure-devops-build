f702dbe3493dabfb7d428285e297019dad4dda6b
michel.genoud <michelgenoudba@hotmail.fr>
2026-09-16 15:00:09 +0200

    fix(aks): add required node_provisioning_profile block for azurerm v5 (AB#<id>)

--- infra/modules/aks/main.tf
+++ infra/modules/aks/main.tf
@@ -23,6 +23,11 @@ resource "azurerm_kubernetes_cluster" "this" {
     type = "SystemAssigned"
   }
 
+  node_provisioning_profile {
+    mode               = "Manual"
+    default_node_pools = "None"
+  }
+  
   azure_active_directory_role_based_access_control {
     tenant_id          = data.azurerm_client_config.current.tenant_id
     azure_rbac_enabled = true
