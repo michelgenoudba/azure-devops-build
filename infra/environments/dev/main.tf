dbe036c833e265e8e2cadb89926a95c5ee457ef9
michel.genoud <michelgenoudba@hotmail.fr>
2026-09-23 12:14:56 +0200

    fix: corrected dev/main.tf syntax

--- infra/environments/dev/main.tf
+++ infra/environments/dev/main.tf
@@ -90,16 +90,16 @@ module "app_insights" {
 module "monitoring_alerts" {
   source = "../../modules/monitoring-alerts"
 
-  resource_group_name           = data.azurerm_resource_group.main.name
-  location                      = data.azurerm_resource_group.main.location
-  log_analytics_workspace_id    = module.log_analytics_id
-  action_group_name             = "ag-azure-devops-build-mg"
-  action_group_short_name       = "aznotify"
-  notification_email            = var.alter_notification_email 
+  resource_group_name        = data.azurerm_resource_group.main.name
+  location                   = data.azurerm_resource_group.main.location
+  log_analytics_workspace_id = module.log_analytics.id
+  action_group_name          = "ag-azure-devops-build-mg"
+  action_group_short_name    = "aznotify"
+  notification_email         = var.alert_notification_email
 
   tags = {
-    projects        = "azure-devops-build"
-    environment     = "dev"
+    project     = "azure-devops-build"
+    environment = "dev"
   }
 }
 
